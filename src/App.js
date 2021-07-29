f88608de8e01752f9e46e57ae2af586c76f09465
Susmit Basu <bosesusmit2006@gmail.com>
2021-07-29 22:04:51 +0800

    App init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Enough learning </header>
+			Lets write code...
+		</div>
+	);
+}
+
+export default App;
